329c1b184bef111ec444c35af4da707f011e518d
DanyloTymets <56023366+DanyloTymets@users.noreply.github.com>
2022-05-16 20:22:40 +0300

    Updated DB
    updated news and events

--- lnu-news.sql
+++ lnu-news.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1
--- Час створення: Трв 14 2022 р., 22:30
+-- Час створення: Трв 16 2022 р., 19:21
 -- Версія сервера: 10.4.10-MariaDB
 -- Версія PHP: 7.3.12
 
@@ -148,13 +148,13 @@ CREATE TABLE `events` (
 --
 
 INSERT INTO `events` (`event_id`, `e_cat_id`, `cat_id`, `date`, `event_title`, `event_img1`, `event_keywords`, `event_desc`) VALUES
-(1, 1, 1, '2021-05-30 10:00:00', 'New test system', 'lnu.jpg', 'News', '<p>Введено нову систему тестування</p>                          '),
-(2, 2, 2, '2021-05-29 10:00:00', 'Випускний для бакалаврів 2021', 'graduation.jpg', 'Event', '<p>Випускний для бакалаврів 2021 для грув ПМІ-41,42,43</p>                          '),
-(3, 2, 2, '2021-05-30 10:00:00', 'Науковий семінар', 'ns.jpg', 'Event', '<p>Спільний науковий семінар філологічного факультету Львівського національного університету імені Івана Франка та відділу соціолінгвістики Інституту української мови НАН України</p>'),
-(4, 1, 1, '2021-05-28 10:00:00', 'Науковий семінар', 'lnu.jpg', 'News', '<p>Науковий семінар</p>'),
-(5, 2, 2, '2021-06-01 11:00:00', 'Барди у парку', 'bards.jpg', 'Event', '<p>Красива природа, приємна компанія та музика! Запрошуємо всіх на барди у парку навпроти ЛНУ! Якщо ви маєте музичний інструмент (гітару/укулеле/саксофон і т.д.), добре граєте, то реєструйтесь (самі чи з друзями), щоб стати виконавцем (виконавцями) на бардах.</p>'),
-(6, 1, 1, '2021-07-15 10:00:00', 'Зміни в роботі', 'lnu.jpg', 'News', '<p>Зміни в роботі</p>'),
-(7, 2, 2, '2021-06-05 12:00:00', 'Mafia', 'mafia.jpg', 'Event', '<p>Mafia one of the most popular game in the world Mafia one of the most popular game in the world Mafia one of the most popular game in the world Mafia one of the most popular game in the world Mafia one of the most popular game in the world Mafia one of the most popular game in the world</p>');
+(1, 1, 1, '2022-05-16 17:19:30', 'New test system', 'lnu.jpg', 'News', '                            <p>Введено нову систему тестування</p>                                                    '),
+(2, 2, 2, '2022-05-16 17:18:03', 'Конференція трудового колективу та засідання Вченої ради', 'graduation.jpg', 'Event', '                            <p>В середу 13 квітня 2022 року о 15 год. (дистанційно в MS Teams) відбудеться  Конференція трудового колективу факультету\r\n</br>\r\n</br>\r\nПосилання на нараду: https://cutt.ly/ZFWiAb8.</p>                                                    '),
+(3, 2, 2, '2022-05-16 17:14:52', 'Компанія Noltic запрошує на знайомство з технологією Salesforce', 'ns.jpg', 'Event', '<p >14 квітня о 10:30 запрошуємо на лекцію про роботу з технологією Salesforce та\r\nкар’єрні можливості від Salesforce-розробника компанії Noltic.\r\n</br> </br> \r\nПочаток навчання: 16 червня. Всі деталі тут: courses.noltic.com\r\n</p>                                                    '),
+(4, 1, 1, '2022-05-16 17:08:18', 'Student scholarships for women from Ukraine', 'students2.jpg', 'News', '<p>ECCEDU, in cooperation with Dataiku, offers a special scholarship to female refugees from Ukraine to join our IT summer program in Prague. The scholarship aims to help the women during these challenging times, not only to learn about technology and AI but also to gain adequate expertise in the field to be granted a work opportunity after finishing the program.\r\n</br>\r\n</br>\r\nThe deadline for the application is May 7, 2022.\r\n</p>                          '),
+(5, 2, 2, '2022-05-16 17:04:13', 'Барди у парку', 'bards.jpg', 'Event', '                            <p>Красива природа, приємна компанія та музика! Запрошуємо всіх на барди у парку навпроти ЛНУ! Якщо ви маєте музичний інструмент (гітару/укулеле/саксофон і т.д.), добре граєте, то реєструйтесь (самі чи з друзями), щоб стати виконавцем (виконавцями) на бардах.</p>                          '),
+(6, 1, 1, '2022-05-16 16:24:13', 'В Університеті обговорили особливості реалізації прав у сфері праці та соціального захисту в умовах воєнного стану', 'lnu.jpg', 'News', '<p>12 травня 2022 року у Львівському національному університеті імені Івана Франка відбувся круглий стіл «Особливості реалізації прав у сфері праці та соціального захисту в умовах воєнного стану». Захід провели у змішаному форматі з використанням платформи Zoom. Організаторами круглого столу є кафедра соціального права юридичного факультету Львівського національного університету імені Івана Франка, кафедра трудового права Національного юридичного університету імені Ярослава Мудрого, кафедра цивільного права і процесу Донецького національного університету імені Василя Стуса та Громадська організація «Український центр соціально-правових досліджень».</p>\r\n</br>\r\n<p>Наукова проблематика круглого столу охопила низку важливих та актуальних питань. Учасники засідання, зокрема, обговорили питання законодавчих новацій і правозастосування робочого часу і часу відпочинку в умовах воєнного стану, питання реалізації конституційного права на працю в умовах воєнного стану, вплив на стан трудових відносин призову або вступу сторони трудового договору на військову службу. </p>                                                    '),
+(7, 2, 2, '2022-05-16 17:02:10', 'Міжнародна Студентська Наукова Конференція з Прикладної Математики та Комп’ютерних Наук', 'ns.jpg', 'Event', '                                                                                    <p>The annual International Student Scientific Conference of Applied Mathematics and Computer Science brings together students, graduate students and teachers from various educational institutions, not only from Lviv, but also from all over Ukraine and abroad.\r\n</br>\r\n</br>\r\nStudents, IT specialists, as well as young scientists and invited guests doing research in the field of applied mathematics and computer science are invited to participate.\r\n</br>\r\n</br>\r\nConference languages are Ukrainian and English.\r\n</br>\r\n</br>\r\nParticipation is free.</p>                                                                              ');
 
 -- --------------------------------------------------------
 
@@ -175,7 +175,13 @@ CREATE TABLE `events_comments` (
 --
 
 INSERT INTO `events_comments` (`Id`, `CustomerID`, `EventId`, `Message`, `Date`) VALUES
-(1, 1, 7, 'asgjvcjabwfkbwf', '2022-05-14');
+(1, 1, 7, 'asgjvcjabwfkbwf', '2022-05-14'),
+(2, 1, 7, 'awfawfawfawfw', '2022-05-15'),
+(3, 1, 3, 'Interesting', '2022-05-15'),
+(4, 1, 3, 'gxgxdh', '2022-05-15'),
+(6, 1, 7, 'awfaffwfawfawfzcsvzcsvzsvzsvzsv', '2022-05-15'),
+(7, 1, 4, 'afsezGEGSEG', '2022-05-15'),
+(9, 1, 4, 'aFfafFawf', '2022-05-15');
 
 -- --------------------------------------------------------
 
@@ -331,7 +337,7 @@ ALTER TABLE `events`
 -- AUTO_INCREMENT для таблиці `events_comments`
 --
 ALTER TABLE `events_comments`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT для таблиці `event_categories`
